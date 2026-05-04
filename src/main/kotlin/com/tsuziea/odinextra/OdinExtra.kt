d4cbc2faef82459754c2129ea05c1d8535862ed3
Tsuziea <tsuziea@outlook.com>
2026-05-04 10:57:12 +0800

    feat: AutoGFS

--- src/main/kotlin/com/tsuziea/odinextra/OdinExtra.kt
+++ src/main/kotlin/com/tsuziea/odinextra/OdinExtra.kt
@@ -6,6 +6,7 @@ import com.odtheking.odin.features.ModuleManager
 import com.tsuziea.odinextra.features.impl.boss.AutoTerms
 import com.tsuziea.odinextra.features.impl.boss.DragonsHp
 import com.tsuziea.odinextra.features.impl.boss.FuckDiorite
+import com.tsuziea.odinextra.features.impl.dungeon.AutoGFS
 import com.tsuziea.odinextra.features.impl.render.Fullbright
 import com.tsuziea.odinextra.features.impl.render.NameChanger
 import com.tsuziea.odinextra.features.impl.skyblock.AutoClicker
@@ -21,7 +22,7 @@ object OdinExtra : ClientModInitializer {
             // boss
             AutoTerms, DragonsHp, FuckDiorite,
             // render
-            Fullbright, NameChanger,
+            Fullbright, NameChanger, AutoGFS,
             // skyblock
             AutoClicker
         )
