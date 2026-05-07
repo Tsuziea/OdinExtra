b64e0ae1d7b5a2ab0afdb87ab88a9c2ef4197648
Tsuziea <tsuziea@outlook.com>
2026-05-08 00:53:30 +0800

    fix(AutoTerms): remove queue click.

--- src/main/kotlin/com/tsuziea/odinextra/features/impl/boss/AutoTerms.kt
+++ src/main/kotlin/com/tsuziea/odinextra/features/impl/boss/AutoTerms.kt
@@ -2,8 +2,8 @@ package com.tsuziea.odinextra.features.impl.boss
 
 import com.odtheking.odin.clickgui.settings.impl.BooleanSetting
 import com.odtheking.odin.clickgui.settings.impl.NumberSetting
-import com.odtheking.odin.events.ScreenEvent
 import com.odtheking.odin.events.TerminalEvent
+import com.odtheking.odin.events.TickEvent
 import com.odtheking.odin.events.core.on
 import com.odtheking.odin.events.core.onReceive
 import com.odtheking.odin.features.Module
@@ -12,7 +12,6 @@ import com.odtheking.odin.utils.skyblock.dungeon.M7Phases
 import com.odtheking.odin.utils.skyblock.dungeon.terminals.TerminalTypes
 import com.odtheking.odin.utils.skyblock.dungeon.terminals.TerminalUtils.currentTerm
 import net.minecraft.network.protocol.game.ClientboundOpenScreenPacket
-import java.util.ArrayDeque
 
 object AutoTerms : Module (
     name = "Auto Terms",
@@ -25,11 +24,9 @@ object AutoTerms : Module (
     private var lastClickAt = 0L
     private var containerUpdated = false
     private var lastContainerUpdateAt = 0L
-    private val queue = ArrayDeque<Int>()
-    private var queued = false
 
     init {
-        on<ScreenEvent.Render> {
+        on<TickEvent.Start> {
             with(currentTerm ?: return@on) {
                 if (solution.isEmpty()) return@on
 
@@ -41,8 +38,6 @@ object AutoTerms : Module (
                     return@on
                 }
 
-                if (!queued) queueClicks()
-
                 if (type != TerminalTypes.MELODY && !containerUpdated) return@on
                 if (now - lastClickAt < delay) return@on
 
@@ -51,16 +46,15 @@ object AutoTerms : Module (
                 containerUpdated = false
 
                 val slotIndex = solution.firstOrNull() ?: return@on
-                val queueIndex = queue.pollFirst() ?: return@on
 
                 when (type) {
                     TerminalTypes.MELODY -> click(solution.find { it % 9 == 7 } ?: return@on, 2, false)
                     TerminalTypes.RUBIX -> click(
-                        queueIndex,
+                        slotIndex,
                         if (solution.count { it == slotIndex } >= 3) 1 else 2,
                         false)
 
-                    else -> click(queueIndex, 2, false)
+                    else -> click(slotIndex, 2, false)
                 }
             }
         }
@@ -68,7 +62,6 @@ object AutoTerms : Module (
         on<TerminalEvent.Open> {
             firstClick = true
             lastClickAt= System.currentTimeMillis()
-            queued = false
         }
 
         onReceive<ClientboundOpenScreenPacket> {
@@ -76,39 +69,5 @@ object AutoTerms : Module (
             containerUpdated = true
             lastContainerUpdateAt = System.currentTimeMillis()
         }
-    }
-
-    private fun queueClicks() {
-        val term = currentTerm ?: return
-        queue.clear()
-
-        val humanClickOrder = when (term.type) {
-            TerminalTypes.PANES,
-            TerminalTypes.STARTS_WITH,
-            TerminalTypes.SELECT -> {
-                term.solution.sortedWith(
-                    compareBy<Int> { it / 9 }.thenBy { slot ->
-                        val row = slot / 9
-                        val col = slot % 9
-                        if (row == 2 || row == 4) -col else col
-                    }
-                )
-            }
-
-            TerminalTypes.RUBIX -> {
-                term.solution.groupingBy { it }.eachCount().flatMap { (v, c) ->
-                    when (c) {
-                        3 -> listOf(v, v)
-                        4 -> listOf(v)
-                        else -> List(c) { v }
-                    }
-                }
-            }
-
-            else -> term.solution
-        }
-
-        queue.addAll(humanClickOrder)
-        queued = true
     }
 }
